ceaf069566d0e1cb37a649fe8984c396410e245c
Mike Nguyen <nguyenduchung0609@gmail.com>
2018-08-29 06:51:09 +0800

    add node module
    initialize some standard package for testing ES6

---
+++ es6/async-repo.js
@@ -0,0 +1,10 @@
+require('isomorphic-fetch')
+
+async function getRepo(username, number) {
+	let response = await fetch(`https://api.github.com/users/${username}`)
+	let data = await response.json()
+
+	console.log(data)
+}
+
+getRepo('mikenguyen69', 5)
